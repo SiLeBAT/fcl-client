b1ebe6fe5f9aa89f44955625787d056f1e07dca9
Christian Thoens <christian.thoens@gmail.com>
2016-06-30 12:46:04 +0000

    style updated

--- app/scripts/main_component.js
+++ app/scripts/main_component.js
@@ -27,19 +27,20 @@ angular.module('app').component('main', {
         };
     },
     template: '' +
-        '<md-toolbar layout="row" class="md-whiteframe-4dp">' +
-        '   <md-button class="menu" aria-label="Menu" ng-click="$ctrl.toogleList()" hide-gt-sm>' +
-        '       <md-icon md-svg-icon="menu"></md-icon>' +
-        '   </md-button>' +
-        '   <div class="md-toolbar-tools">' +
-        '       <span>FoodChain-Lab</span>' +
+        '<div class="container" layout="column">' +
+        '   <md-toolbar layout="row" class="md-whiteframe-4dp">' +
+        '       <md-button class="menu" aria-label="Menu" ng-click="$ctrl.toogleList()" hide-gt-sm>' +
+        '           <md-icon md-svg-icon="menu"></md-icon>' +
+        '       </md-button>' +
+        '       <div class="md-toolbar-tools">' +
+        '           <span>FoodChain-Lab</span>' +
+        '       </div>' +
+        '   </md-toolbar>' +
+        '   <div class="container" layout="row" flex>' +
+        '       <md-sidenav md-component-id="sidenav" class="md-whiteframe-4dp" md-is-locked-open="$mdMedia(\'gt-sm\')" flex>' +
+        '           <settings node-size="$ctrl.nodeSize" on-change="$ctrl.onNodeSizeChange(value)"></settings>' +
+        '       </md-sidenav>' +
+        '       <div id="graph" ng-style="$ctrl.graphStyle" flex></div>' +
         '   </div>' +
-        '</md-toolbar>' +
-        '' +
-        '<div class="container" layout="row" flex>' +
-        '   <md-sidenav md-component-id="sidenav" class="md-whiteframe-4dp" md-is-locked-open="$mdMedia(\'gt-sm\')" flex>' +
-        '       <settings node-size="$ctrl.nodeSize" on-change="$ctrl.onNodeSizeChange(value)"></settings>' +
-        '   </md-sidenav>' +
-        '   <div id="graph" ng-style="$ctrl.graphStyle" flex></div>' +
         '</div>'
 });
