2a818fbfe0747ca93d38993443fd8e5810801d38
Christian Thoens <christian.thoens@gmail.com>
2016-07-05 13:00:41 +0000

    minor changes

--- app/scripts/data_provider.js
+++ app/scripts/data_provider.js
@@ -1,20 +1,37 @@
 'use strict';
 
-/*global angular, btoa*/
+/*global angular, btoa, $*/
 
 angular.module('app').factory('dataProvider', function($resource) {
-    // return $resource('data/bbk.json');
-    return $resource('https://foodrisklabs.bfr.bund.de/busstop/rest/items/rdt_json', {}, {
-        login: {
-            method: 'GET',
-            dataType: "jsonp",
-            crossDomain: true,
-            isArray: false,
-            headers: {
-                'Authentication': function() {
-                    return btoa('bfr_test:Ifupofetu843');
-                }
-            }
+    var authent = btoa('bfr_test:Ifupofetu843');
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://foodrisklabs.bfr.bund.de/busstop/rest/items/rdt_json',
+        //crossDomain: true,
+        dataType: 'json',
+        headers: {
+            'Authentication': "Basic " + authent
+        },
+        success: function(responseData, textStatus, jqXHR) {
+            console.log(responseData);
+        },
+        error: function(responseData, textStatus, errorThrown) {
+            console.warn(responseData, textStatus, errorThrown);
         }
     });
+
+    return $resource('data/bbk.json');
+    // return $resource('https://foodrisklabs.bfr.bund.de/busstop/rest/items/rdt_json', {}, {
+    //     login: {
+    //         method: 'GET',
+    //         dataType: "jsonp",
+    //         isArray: false,
+    //         headers: {
+    //             'Authentication': function() {
+    //                 return btoa('bfr_test:Ifupofetu843');
+    //             }
+    //         }
+    //     }
+    // });
 });
--- app/scripts/test_component.js
+++ app/scripts/test_component.js
@@ -19,7 +19,7 @@ angular.module('app').component('test', {
         '<div class="container" layout="row" flex>' +
         '   <md-sidenav md-component-id="sidenav" class="md-whiteframe-4dp" md-is-locked-open="$mdMedia(\'gt-sm\')">' +
         '   </md-sidenav>' +
-        '   <md-content layout-padding>' +
+        '   <md-content layout-padding flex>' +
         '       <md-list>' +
         '           <md-list-item ng-repeat="station in $ctrl.stations" class="noright">' +
         '               <p>{{station.data.name}}</p>' +
