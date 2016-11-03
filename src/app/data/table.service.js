8c5af9a325dae43bc8e770c5b0975a8f03014a03
Christian Thoens <christian.thoens@gmail.com>
2016-11-03 22:44:00 +0000

    coloring in tables updated

---
+++ src/app/data/table.service.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/*global angular*/
+
+angular.module('app').service('tableService', function(dataService) {
+
+    var _this = this;
+
+    _this.getElementsOnTrace = function(elements) {
+        return elements.filter(function(elements) {
+            return elements.data.forward || elements.data.backward;
+        });
+    };
+
+    _this.getStyle = function(element) {
+        var colors = [];
+
+        if (element.data.forward) {
+            colors.push(dataService.colorToCss(dataService.COLORS.forward));
+        }
+        if (element.data.backward) {
+            colors.push(dataService.colorToCss(dataService.COLORS.backward));
+        }
+        if (element.data.observed) {
+            colors.push(dataService.colorToCss(dataService.COLORS.observed));
+        }
+        if (element.data.outbreak) {
+            colors.push(dataService.colorToCss(dataService.COLORS.outbreak));
+        }
+
+        if (colors.length === 0) {
+            return {};
+        }
+        else if (colors.length === 1) {
+            return {
+                'background-color': colors[0]
+            };
+        }
+        else {
+            return {
+                'background': 'linear-gradient(to top,' + colors.join(',') + ')'
+            };
+        }
+    };
+});
