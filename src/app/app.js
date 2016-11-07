03d2a0c336724178bd43db846f259e3b230c07e9
Christian Thoens <christian.thoens@gmail.com>
2016-11-07 11:40:26 +0000

    material theme updated

--- src/app/app.js
+++ src/app/app.js
@@ -5,7 +5,7 @@
 angular.module('app', ['ngMaterial', 'ui.router', 'ngResource', 'md.data.table'])
     .config(function($mdIconProvider, $mdThemingProvider) {
         $mdIconProvider.icon('close', './icons/ic_close_white_24px.svg', 24);
-        $mdThemingProvider.theme('default').primaryPalette('indigo').accentPalette('blue');
+        $mdThemingProvider.theme('default').primaryPalette('indigo').accentPalette('red');
     }).config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('graph', {
