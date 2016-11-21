71ef05980899e3e172cc6f37a8296e380181c2e1
Christian Thoens <christian.thoens@gmail.com>
2016-11-21 10:30:42 +0000

    context menu is now contrained to viewport

--- src/app/dialogs/dialog.service.js
+++ src/app/dialogs/dialog.service.js
@@ -7,6 +7,9 @@ angular.module('app').service('dialogService', function($mdDialog, $mdPanel) {
     var _this = this;
 
     _this.showContextMenu = function(position, options) {
+        var positions = $mdPanel.newPanelPosition().relativeTo(angular.element(document.body))
+            .addPanelPosition().left(position.x + 'px').top(position.y + 'px');
+
         $mdPanel.open({
             controller: function($scope, mdPanelRef) {
                 $scope.options = options;
@@ -19,7 +22,7 @@ angular.module('app').service('dialogService', function($mdDialog, $mdPanel) {
             },
             template: '<md-content><context-menu options="options" on-select="select(value)"></context-menu></md-content>',
             attachTo: angular.element(document.body),
-            position: $mdPanel.newPanelPosition().absolute().left(position.x + 'px').top(position.y + 'px'),
+            position: positions,
             clickOutsideToClose: true,
             clickEscapeToClose: true,
             hasBackdrop: true
