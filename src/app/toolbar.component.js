ed1650da6e39712ecf742dd000f4de64847e1038
Christian Thoens <christian.thoens@gmail.com>
2016-11-22 16:40:25 +0000

    bugfix in toolbar

--- src/app/toolbar.component.js
+++ src/app/toolbar.component.js
@@ -3,10 +3,15 @@
 /*global angular*/
 
 angular.module('app').component('toolbar', {
-    controller: function($state) {
+    controller: function($state, $rootScope) {
         var _this = this;
 
         _this.currentNavItem = $state.current.name;
+
+        $rootScope.$on('$stateChangeStart',
+            function(event, toState, toParams, fromState, fromParams, options) {
+                _this.currentNavItem = toState.name;
+            });
     },
     templateUrl: 'app/toolbar.component.html'
 });
