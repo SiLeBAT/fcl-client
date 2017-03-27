46faadadf6a19b32ab986bf67c37d4d4f8772725
thoens <christian.thoens@gmail.com>
2017-03-27 18:13:18 +0200

    legend updated

--- src/app/graph/legend.ts
+++ src/app/graph/legend.ts
@@ -1,19 +1,38 @@
 import {Subject} from 'rxjs/Rx';
 
-export function Legend(params: Subject<string>) {
-  params.subscribe(s => console.log(s));
+export class Legend {
 
-  const mainDiv = document.createElement('div');
+  constructor(params: Subject<string>) {
+    params.subscribe(s => console.log(s));
 
-  mainDiv.id = 'cy-legend';
-  mainDiv.style.position = 'absolute';
-  mainDiv.style.left = '0%';
-  mainDiv.style.bottom = '0%';
-  mainDiv.style.width = '100px';
-  mainDiv.style.height = '100px';
-  mainDiv.style.backgroundColor = 'rgb(0, 0, 0)';
+    const mainDiv = document.createElement('div');
 
-  this.container().appendChild(mainDiv);
+    const table = document.createElement('table');
 
-  return this;
+    const observed = document.createElement('tr');
+
+    const observedLabel = document.createElement('td');
+
+    observedLabel.innerText = 'Observed';
+
+    const observedColor = document.createElement('td');
+
+    observedColor.style.backgroundColor = 'rgb(0, 0, 255)';
+    observedColor.width = '20px';
+    observedColor.height = '100%';
+
+    observed.appendChild(observedLabel);
+    observed.appendChild(observedColor);
+
+    table.appendChild(observed);
+
+    mainDiv.appendChild(table);
+    mainDiv.id = 'cy-legend';
+    mainDiv.style.position = 'absolute';
+    mainDiv.style.left = '0';
+    mainDiv.style.bottom = '0';
+    mainDiv.style.backgroundColor = 'rgb(200, 200, 200)';
+
+    this['container']().appendChild(mainDiv);
+  }
 }
