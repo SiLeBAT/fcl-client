f1fd90439ea9ad50102fb97144dbe3f557132bbf
thoens <christian.thoens@gmail.com>
2017-04-05 14:31:31 +0200

    gh-pages updated

--- deploy_dist.sh
+++ deploy_dist.sh
@@ -1,5 +1,11 @@
 shopt -s extglob
-rm -r !(deploy_dist.sh|dist|node_modules)
-mv dist/* .
-rm -r dist
+
+if [ -d "dist" ]; then
+  rm -r !(deploy_dist.sh|dist|node_modules)
+  mv dist/* .
+  rm -r dist
+  git add *
+  git commit -am "gh-pages updated"
+  git push
+fi
 
