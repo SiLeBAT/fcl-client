278844055d1d933789be9d797f1f1ff5b6a0ccfb
thoens <christian.thoens@gmail.com>
2017-04-27 11:47:10 +0200

    station-properties updated

--- src/app/dialog/station-properties/station-properties.component.ts
+++ src/app/dialog/station-properties/station-properties.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MD_DIALOG_DATA} from '@angular/material';
-import {D3Service, D3} from 'd3-ng2-service';
+import {D3Service, D3, Selection} from 'd3-ng2-service';
 
 import {DeliveryData, StationData} from '../../util/datatypes';
 import {DataService} from '../../util/data.service';
@@ -42,10 +42,11 @@ export class StationPropertiesComponent implements OnInit {
   private nodeData: NodeDatum[];
   private edgeData: EdgeDatum[];
 
-  private g: any;
-  private defs: any;
-  private edges: any;
-  private dragLine: any;
+  private g: Selection<SVGElement, any, any, any>;
+  private defs: Selection<SVGElement, any, any, any>;
+  private nodesG: Selection<SVGElement, any, any, any>;
+  private edgesG: Selection<SVGElement, any, any, any>;
+  private dragLine: Selection<SVGElement, any, any, any>;
 
   constructor(@Inject(MD_DIALOG_DATA) public data: StationPropertiesData, d3Service: D3Service) {
     this.properties = Object.keys(data.station).filter(key => DataService.PROPERTIES.has(key)).map(key => {
@@ -71,51 +72,55 @@ export class StationPropertiesComponent implements OnInit {
     }];
     this.edgeData = [];
 
-    const svg = this.d3.select('#in-out-connector').append('svg')
+    const svg: Selection<SVGElement, any, any, any> = this.d3
+      .select('#in-out-connector').append<SVGElement>('svg')
       .attr('width', 400).attr('height', 400)
       .on('mouseup', () => this.dragLine.classed(StationPropertiesComponent.HIDDEN, true));
 
-    this.defs = svg.append('svg:defs');
+    this.defs = svg.append<SVGElement>('defs');
     this.appendArrowToDefs('end-arrow');
     this.appendArrowToDefs('end-arrow-hover');
 
-    this.g = svg.append('g');
-    this.dragLine = this.g.append('svg:path')
+    this.g = svg.append<SVGElement>('g');
+    this.dragLine = this.g.append<SVGElement>('path')
       .attr('class', StationPropertiesComponent.EDGE + ' ' + StationPropertiesComponent.HIDDEN);
-    this.edges = this.g.append('g').selectAll('g');
+    this.edgesG = this.g.append<SVGElement>('g');
+    this.nodesG = this.g.append<SVGElement>('g');
 
     this.addNodes();
     this.updateEdges();
   }
 
   private appendArrowToDefs(id: string) {
-    this.defs.append('svg:marker')
+    this.defs.append('marker')
       .attr('id', id)
       .attr('viewBox', '0 -5 10 10')
       .attr('refX', 7)
       .attr('markerWidth', 3.5)
       .attr('markerHeight', 3.5)
       .attr('orient', 'auto')
-      .append('svg:path')
+      .append('path')
       .attr('d', 'M0,-5L10,0L0,5');
   }
 
   private addNodes() {
     let hoverD;
-    const nodes = this.g.append('g').selectAll('g').data(this.nodeData, d => d.id).enter().append('g');
+    const updateDragLinePosition = d => {
+      const mousePos = this.d3.mouse(document.getElementById('in-out-connector'));
+
+      this.dragLine.attr('d', 'M' + (d.x + 50) + ',' + d.y + 'L' + mousePos[0] + ',' + mousePos[1]);
+    };
+    const nodes = this.nodesG.selectAll<SVGElement, NodeDatum>('g').data(this.nodeData, d => d.id).enter().append('g');
 
     nodes.classed(StationPropertiesComponent.NODE, true)
       .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')').append('circle').attr('r', '50');
 
     nodes.call(this.d3.drag<Element, NodeDatum>()
       .on('start', d => {
+        updateDragLinePosition(d);
         this.dragLine.classed(StationPropertiesComponent.HIDDEN, false);
       })
-      .on('drag', d => {
-        const mousePos = this.d3.mouse(document.getElementById('in-out-connector'));
-
-        this.dragLine.attr('d', 'M' + (d.x + 50) + ',' + d.y + 'L' + mousePos[0] + ',' + mousePos[1]);
-      })
+      .on('drag', updateDragLinePosition)
       .on('end', d => {
         if (hoverD != null && hoverD !== d) {
           const newEdge: EdgeDatum = {
@@ -147,11 +152,12 @@ export class StationPropertiesComponent implements OnInit {
   }
 
   private updateEdges() {
-    this.edges.enter().remove();
-    this.edges = this.edges.data(this.edgeData, d => String(d.source.id) + '+' + String(d.target.id));
-    this.edges.attr('d', d => 'M' + (d.source.x + 50) + ',' + d.source.y + 'L' + (d.target.x - 50) + ',' + d.target.y);
-    this.edges.enter().append('path').classed(StationPropertiesComponent.EDGE, true)
-      .attr('d', d => 'M' + d.source.x + ',' + d.source.y + 'L' + (d.target.x - 50) + ',' + d.target.y)
+    const edges = this.edgesG.selectAll<SVGElement, EdgeDatum>('path')
+      .data(this.edgeData, d => String(d.source.id) + '+' + String(d.target.id));
+
+    edges.exit().remove();
+    edges.enter().append('path').classed(StationPropertiesComponent.EDGE, true)
+      .attr('d', d => 'M' + (d.source.x + 50) + ',' + d.source.y + 'L' + (d.target.x - 50) + ',' + d.target.y)
       .on('click', d => {
         this.edgeData.splice(this.edgeData.indexOf(d), 1);
         this.updateEdges();
