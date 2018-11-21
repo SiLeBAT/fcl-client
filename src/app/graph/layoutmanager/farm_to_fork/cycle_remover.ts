c5bf3fe56711f669d9ba49f2e146732ca63fa6e5
Marco Ruegen <marco.ruegen@bfr.bund.de>
2018-11-21 08:49:11 +0100

    tslint compliance

--- src/app/graph/layoutmanager/farm_to_fork/cycle_remover.ts
+++ src/app/graph/layoutmanager/farm_to_fork/cycle_remover.ts
@@ -1,72 +1,61 @@
+import * as _ from 'lodash';
 import {Graph, Vertex, Edge} from './data_structures';
 import {BusinessTypeRanker} from './business_type_ranker';
 
 export function removeCycles(graph: Graph, typeRanker: BusinessTypeRanker) {
-  let cycleRemover: CycleRemover = new CycleRemover();
-  cycleRemover.removeCycles(graph, typeRanker);
+    const cycleRemover: CycleRemover = new CycleRemover();
+    cycleRemover.removeCycles(graph, typeRanker);
 }
 
 class CycleRemover {
-  //private Stack: Vertex[];
-  private isMarked: boolean[];
-  private isStacked: boolean[];
-  private vertices: Vertex[];
-  private removedCycleEdges: Edge[];
-  
-  constructor() {}
-  
-  init(graph: Graph) {
-    this.vertices = graph.vertices;
-    this.removedCycleEdges = [];
-    this.isMarked = [];
-    this.isStacked = [];
-    //this.Stack = [];
-    for(let i: number = graph.vertices.length; i>=0; --i) this.isMarked[i] = false;
-    for(let i: number = graph.vertices.length; i>=0; --i) this.isStacked[i] = false;
-  }
-  
-  removeCycles(graph: Graph, typeRanker: BusinessTypeRanker) {
-    this.init(graph);
-    for(let vertex of graph.vertices) this.dfsRemove(vertex);
-  }
-  
-  private dfsRemove(vertex: Vertex) {
-    if(this.isMarked[vertex.index]) return;
-    
-    this.isMarked[vertex.index] = true; //.add(vertex)
-    this.isStacked[vertex.index] = true;
-    //this.Stack.push(vertex); // = true; //.add(vertex)
-    //let removedOutNodes: Set<Vertex> = new Set();
-    let reversedOutEdges: Set<Edge> = new Set();
-    
-    
-    for(let iE: number = 0; iE<vertex.outEdges.length; iE++) {
-      const outEdge: Edge = vertex.outEdges[iE];
-      if (this.isStacked[outEdge.target.index]) {
-        // reverse edge
-        //removedOutNodes.push(outEdge.target);
-        reversedOutEdges.add(outEdge);
-      } else if (!this.isMarked[outEdge.target.index]) {
-        this.dfsRemove(outEdge.target);
-      }
-    }
-    
-    if(reversedOutEdges.size>0) {
-      vertex.outEdges = vertex.outEdges.filter(e => !reversedOutEdges.has(e));
-      
-      reversedOutEdges.forEach(edge => {
-        edge.target.inEdges = edge.target.inEdges.filter(e => !reversedOutEdges.has(e));
-        //this.removedCycleEdges.push(edge);
-      });
 
-      reversedOutEdges.forEach(oldEdge => {
-        const newEdge: Edge = new Edge(oldEdge.target, oldEdge.source, false);
-        newEdge.source.outEdges.push(newEdge);
-        newEdge.target.inEdges.push(newEdge);
-      });
+    private isMarked: boolean[];
+    private isStacked: boolean[];
+
+    constructor() {}
+
+    init(graph: Graph) {
+        this.isMarked = _.fill(Array(graph.vertices.length), false);
+        this.isStacked = _.fill(Array(graph.vertices.length), false);
     }
-  
-    this.isStacked[vertex.index] = false;
-    //this.Stack.pop();
-  }
+
+    removeCycles(graph: Graph, typeRanker: BusinessTypeRanker) {
+        this.init(graph);
+        for (const vertex of graph.vertices) { this.dfsRemove(vertex); }
+    }
+
+    private dfsRemove(vertex: Vertex) {
+        if (this.isMarked[vertex.index]) { return; }
+
+        this.isMarked[vertex.index] = true;
+        this.isStacked[vertex.index] = true;
+
+        const reversedOutEdges: Set<Edge> = new Set();
+
+        for (let iE = 0; iE < vertex.outEdges.length; iE++) {
+            const outEdge: Edge = vertex.outEdges[iE];
+            if (this.isStacked[outEdge.target.index]) {
+                // reverse edge
+                reversedOutEdges.add(outEdge);
+            } else if (!this.isMarked[outEdge.target.index]) {
+                this.dfsRemove(outEdge.target);
+            }
+        }
+
+        if (reversedOutEdges.size > 0) {
+            vertex.outEdges = vertex.outEdges.filter(e => !reversedOutEdges.has(e));
+
+            reversedOutEdges.forEach(edge => {
+                edge.target.inEdges = edge.target.inEdges.filter(e => !reversedOutEdges.has(e));
+            });
+
+            reversedOutEdges.forEach(oldEdge => {
+                const newEdge: Edge = new Edge(oldEdge.target, oldEdge.source, false);
+                newEdge.source.outEdges.push(newEdge);
+                newEdge.target.inEdges.push(newEdge);
+            });
+        }
+
+        this.isStacked[vertex.index] = false;
+    }
 }
--- src/app/graph/layoutmanager/farm_to_fork/farm_to_fork.ts
+++ src/app/graph/layoutmanager/farm_to_fork/farm_to_fork.ts
@@ -4,162 +4,141 @@ import * as _ from 'lodash';
 import {Graph, Vertex, Edge} from './data_structures';
 import {removeCycles} from './cycle_remover';
 import {assignLayers} from './layer_assigner';
-import {sortVertices} from './vertex_sorter';
-import {positionVertices} from './vertex_positioner_lp';
 import {BusinessTypeRanker} from './business_type_ranker';
 import {scaleToSize } from './shared';
 import {sortAndPosition} from './vertex_sorter_and_positioner';
 
 export function FarmToForkLayout(options) {
-  this.options = options;
+    this.options = options;
 }
 
 function getDateNumber(date: Date): number {
-  return date.getMilliseconds() + date.getSeconds()*1000 + date.getMinutes()*60000 + date.getHours()*3600000;
+    return date.getMilliseconds() + date.getSeconds() * 1000 + date.getMinutes() * 60000 + date.getHours() * 3600000;
 }
 
 function getElapsedTime(dateStart: Date, dateEnd: Date): number {
-  return getDateNumber(dateEnd)-getDateNumber(dateStart);
+    return getDateNumber(dateEnd) - getDateNumber(dateStart);
 }
 
 FarmToForkLayout.prototype.run = function () {
-  new FarmToForkLayoutClass(this).run();
+    new FarmToForkLayoutClass(this).run();
 };
 
 class FarmToForkLayoutClass {
-  
-  private static DEFAULTS = {
-    fit: true
-  };
-  
-  private layout: any;
-  private options: any;
-  
-  constructor(layout: any) {
-    this.layout = layout;
-    this.options = {};
-    
-    for (const key of Object.keys(layout.options)) {
-      this.options[key] = layout.options[key];
+
+    private static DEFAULTS = {
+        fit: true
+    };
+
+    private layout: any;
+    private options: any;
+
+    constructor(layout: any) {
+        this.layout = layout;
+        this.options = {};
+
+        for (const key of Object.keys(layout.options)) {
+            this.options[key] = layout.options[key];
+        }
+
+        for (const key of Object.keys(FarmToForkLayoutClass.DEFAULTS)) {
+            if (!this.options.hasOwnProperty(key)) {
+                this.options[key] = FarmToForkLayoutClass.DEFAULTS[key];
+            }
+        }
     }
-    
-    for (const key of Object.keys(FarmToForkLayoutClass.DEFAULTS)) {
-      if (!this.options.hasOwnProperty(key)) {
-        this.options[key] = FarmToForkLayoutClass.DEFAULTS[key];
-      }
+
+    run() {
+        const cy = this.options.cy;
+        const width: number = cy.width();
+        const height: number = cy.height();
+        const graph = new Graph();
+        const vertices: Map<string, Vertex> = new Map();
+        const typeRanker: BusinessTypeRanker = new BusinessTypeRanker([], [], []);
+
+        cy.nodes().forEach(node => {
+            const v: Vertex = new Vertex;
+            v.typeCode = typeRanker.getBusinessTypeCode(node['data']['typeOfBusiness']);
+            v.size = node.height();
+            v.name = node.data('name');
+            vertices.set(node.id(), v);
+            graph.insertVertex(v);
+        });
+
+        const vertexDistance: number = Math.min(...graph.vertices.map(v => v.size)) / 2;
+        cy.edges().forEach(edge => {
+            graph.insertEdge(vertices.get(edge.source().id()), vertices.get(edge.target().id()));
+        });
+
+        const layoutManager: FarmToForkLayouter = new FarmToForkLayouter(graph, typeRanker);
+
+        layoutManager.layout(width, height, vertexDistance);
+
+        cy.nodes().layoutPositions(this.layout, this.options, node => {
+            const vertex = vertices.get(node.id());
+
+
+            return {
+                x: vertex.x,
+                y: vertex.y
+            };
+        });
+
+        if (this.options.fit) {
+            //cy.fit();
+        }
     }
-  }
-  
-  run() {
-    const cy = this.options.cy;
-    const width: number = cy.width();
-    const height: number = cy.height();
-    const graph = new Graph();
-    const vertices: Map<string, Vertex> = new Map();
-    const typeRanker: BusinessTypeRanker = new BusinessTypeRanker([],[],[]);
-    let tmpH: [number, number] = [100000,-100000];
-    let tmpV: [number, number] = [100000,-100000];
-    cy.nodes().forEach(node => {
-      let v: Vertex = new Vertex;
-      v.typeCode = typeRanker.getBusinessTypeCode(node['data']['typeOfBusiness']);
-      v.size = node.height();
-      v.name = node.data('name');
-      //v.topMargin = node.height()/2;
-      //v.bottomMargin = v.topMargin;
-      vertices.set(node.id(), v);
-      graph.insertVertex(v);
-      tmpH = [Math.min(node.data('position').x, tmpH[0]), Math.max(node.data('position').x, tmpH[1])];
-      tmpV = [Math.min(node.data('position').y, tmpV[0]), Math.max(node.data('position').y, tmpV[1])];
-    });
-    
-    const vertexDistance: number = Math.min(...graph.vertices.map(v=>v.size))/2;
-    cy.edges().forEach(edge => {
-      graph.insertEdge(vertices.get(edge.source().id()), vertices.get(edge.target().id()));
-    });
-    
-    const layoutManager: FarmToForkLayouter = new FarmToForkLayouter(graph, typeRanker);
-    
-    layoutManager.layout(width, height, vertexDistance);
-    
-    cy.nodes().layoutPositions(this.layout, this.options, node => {
-      const vertex = vertices.get(node.id());
-      
-      
-      return {
-        x: vertex.x,
-        y: vertex.y
-      };
-    });
-    
-    if (this.options.fit) {
-      //cy.fit();
-    }
-  }
 }
 
 class FarmToForkLayouter {
-  constructor(private graph: Graph, private typeRanker: BusinessTypeRanker) {};
-  
-  layout(width: number, height: number, vertexDistance: number) {
-    this.simplifyGraph();
-    this.correctEdges();
-    this.simplifyGraph();
-    let startTime: Date = new Date();
-    removeCycles(this.graph, this.typeRanker);
-    
-    //console.log('removeCycles: ' + getElapsedTime(startTime,new Date()).toString() + ' ms');
-    this.simplifyGraph();
-    startTime = new Date();
-    assignLayers(this.graph, this.typeRanker);
-    //console.log('assignLayers: ' + getElapsedTime(startTime,new Date()).toString() + ' ms');
-    //removeEdgesWithinLayers(this.graph);
-    startTime = new Date();
-    sortAndPosition(this.graph, vertexDistance);
-    //sortVertices(this.graph);
-    //console.log('sortAndPositionVertices: ' + getElapsedTime(startTime,new Date()).toString() + ' ms');
-    //startTime = new Date();
-    //positionVertices(this.graph.layers, vertexDistance);
-    //console.log('positionVertices: ' + getElapsedTime(startTime,new Date()).toString() + ' ms');
-    scaleToSize(this.graph, width, height);
-    //this.computePositions();
-  }
-  
-  /*simplifyGraph() {
-    let edges: Edge[] = [];
-    for(let vertex of this.graph.vertices) {
-      edges = edges.concat(edges, vertex.outEdges);
-      vertex.outEdges = [];
-      vertex.inEdges = [];
-    }
-    edges = _.uniqWith(edges, (a,b) => {return a.source===b.source && a.target===b.target});
-    for(let edge of edges) {
-      edge.source.outEdges.push(edge); 
-      edge.target.inEdges.push(edge);
-    }
-  }*/
+    constructor(private graph: Graph, private typeRanker: BusinessTypeRanker) {};
 
-  simplifyGraph() {
-    let edges: Edge[] = [];
-    for(let vertex of this.graph.vertices) vertex.inEdges = [];
-    for(let vertex of this.graph.vertices) {
-       const targets = _.uniq(vertex.outEdges.map(e => e.target.index)).filter(i => i!=vertex.index);
-       const oldEdges: Edge[] = vertex.outEdges;
-       vertex.outEdges = [];
-       for(let iTarget of targets) {
-         const newEdge: Edge = new Edge(vertex, this.graph.vertices[iTarget], false);
-         newEdge.weight = 0;
-         for(let edge of oldEdges) if(edge.target.index===iTarget) {
-           newEdge.weight+= edge.weight;
-          }
-         vertex.outEdges.push(newEdge);
-         newEdge.target.inEdges.push(newEdge);
-       }
-    }
-  }
+    layout(width: number, height: number, vertexDistance: number) {
+        this.simplifyGraph();
+        this.correctEdges();
+        this.simplifyGraph();
+        let startTime: Date = new Date();
+        removeCycles(this.graph, this.typeRanker);
 
-  correctEdges() {
-    let edgesToInvert: Edge[] = [];
-    for(let vertex of this.graph.vertices) for(let edge of vertex.outEdges) if(this.typeRanker.compareRanking(vertex, edge.target)>0) edgesToInvert.push(edge);
-    this.graph.invertEdges(edgesToInvert);
-  }
+        // console.log('removeCycles: ' + getElapsedTime(startTime,new Date()).toString() + ' ms');
+        this.simplifyGraph();
+        startTime = new Date();
+        assignLayers(this.graph, this.typeRanker);
+        // console.log('assignLayers: ' + getElapsedTime(startTime,new Date()).toString() + ' ms');
+        // removeEdgesWithinLayers(this.graph);
+        startTime = new Date();
+        sortAndPosition(this.graph, vertexDistance);
+        // console.log('sortAndPositionVertices: ' + getElapsedTime(startTime,new Date()).toString() + ' ms');
+        scaleToSize(this.graph, width, height);
+    }
+
+    simplifyGraph() {
+        for (const vertex of this.graph.vertices) { vertex.inEdges = []; }
+        for (const vertex of this.graph.vertices) {
+            const targets = _.uniq(vertex.outEdges.map(e => e.target.index)).filter(i => i !== vertex.index);
+            const oldEdges: Edge[] = vertex.outEdges;
+            vertex.outEdges = [];
+            for (const iTarget of targets) {
+                const newEdge: Edge = new Edge(vertex, this.graph.vertices[iTarget], false);
+                newEdge.weight = 0;
+                for (const edge of oldEdges) {
+                    if (edge.target.index === iTarget) {
+                        newEdge.weight += edge.weight;
+                    }
+                }
+                vertex.outEdges.push(newEdge);
+                newEdge.target.inEdges.push(newEdge);
+            }
+        }
+    }
+
+    correctEdges() {
+        const edgesToInvert: Edge[] = [];
+        for (const vertex of this.graph.vertices) {
+            for (const edge of vertex.outEdges) {
+                if (this.typeRanker.compareRanking(vertex, edge.target) > 0) { edgesToInvert.push(edge); }
+            }
+        }
+        this.graph.invertEdges(edgesToInvert);
+    }
 }
