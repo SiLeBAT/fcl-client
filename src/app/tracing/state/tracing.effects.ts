0f5d85ef514d144f4663b8ec411f1e5127f80d59
Birgit Lewicki <lewicki.birgit@gmail.com>
2019-05-09 17:48:02 +0200

    Fix unsafe catch linter error in effect.
    Ticket: https://github.com/silebat/fcl-client/issues/135
    Change-Id: I6cea57412d4de7b150bf22ff9c8570890377bc41

--- src/app/tracing/state/tracing.effects.ts
+++ src/app/tracing/state/tracing.effects.ts
@@ -9,7 +9,6 @@ import { Observable, of } from 'rxjs';
 import { DataService } from '../services/data.service';
 import { FclData } from '../util/datatypes';
 import { Store } from '@ngrx/store';
-// import { MainPageService } from '../../main-page/services/main-page.service';
 
 @Injectable()
 export class TracingEffects {
@@ -17,7 +16,6 @@ export class TracingEffects {
         private actions$: Actions,
         private dataService: DataService,
         private alertService: AlertService,
-        // private mainPageService: MainPageService,
         private store: Store<fromTracing.State>
     ) {}
 
@@ -30,7 +28,6 @@ export class TracingEffects {
                 this.dataService.setDataSource(fileList[0]);
                 return Observable.fromPromise(this.dataService.getData()).pipe(
                     map((data: FclData) => new tracingActions.LoadFclDataSuccess(data)),
-                    // tslint:disable-next-line: rxjs-no-unsafe-catch
                     catchError((error) => {
                         this.alertService.error(`Please select a .json file with the correct format!, error: ${error}`);
                         // tslint:disable-next-line:deprecation
@@ -41,12 +38,6 @@ export class TracingEffects {
                 this.alertService.error('Please select a .json file with the correct format!');
                 this.store.dispatch(new tracingActions.LoadFclDataFailure());
             }
-        }),
-        // tslint:disable-next-line: rxjs-no-unsafe-catch
-        catchError((error) => {
-            this.alertService.error(`Please select a .json file with the correct format!, error: ${error}`);
-            // tslint:disable-next-line:deprecation
-            return of(new tracingActions.LoadFclDataFailure());
         })
     );
 }
