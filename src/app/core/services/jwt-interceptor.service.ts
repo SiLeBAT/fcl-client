178f4a0fbdb6e09654ed4f50a59b7c4613cebcd9
Birgit Lewicki <lewicki.birgit@gmail.com>
2019-07-23 13:04:33 +0200

    Change-Id: I47b8622ec4bf510b2ed74ff70690c55a56ba1cac
    Change-Id: Ifbec2fc153a9f42fe237c13539cf1f6675226c0e

--- src/app/core/services/jwt-interceptor.service.ts
+++ src/app/core/services/jwt-interceptor.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, catchError } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 
 import { AlertService } from '../../shared/services/alert.service';
@@ -11,28 +11,23 @@ import * as userActions from './../../user/state/user.actions';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private router: Router,
-                private alertService: AlertService,
+    constructor(private alertService: AlertService,
                 private store: Store<fromUser.State>) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(
-            tap(
-                (event: HttpEvent<any>) => {
-                    // doing nothing
-                },
-                (err: any) => {
-                    if (err instanceof HttpErrorResponse) {
-                        if (err.status === 401) {
-                            this.alertService.error(
-                                ` Nicht authorisiert oder nicht aktiviert.
-                                Wenn bereits registriert, überprüfen Sie bitte Ihre Email auf einen Aktivierungslink.`
-                            );
-                            this.store.dispatch(new userActions.LogoutUser());
-                        }
+            catchError((errorResponse: Error) => {
+                if (errorResponse instanceof HttpErrorResponse) {
+                    if ((errorResponse.status === 401) || (errorResponse.status === 500)) {
+                        this.alertService.error(
+                            `Not authorized or not activated.
+                            If already registered please check your Email for an activation link.`
+                        );
+                        this.store.dispatch(new userActions.LogoutUser());
                     }
                 }
-            )
+                throw HttpErrorResponse;
+            })
         );
     }
 }
