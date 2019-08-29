0a97798a3466927ffa085989ecc2e8a98dab2ac8
Birgit Lewicki <lewicki.birgit@gmail.com>
2019-08-29 14:08:18 +0200

    Add system dialog 'leave page?' for GraphEditor component.
    Ticket: https://github.com/silebat/fcl-client/issues/102
    Change-Id: Ie0037c3049b69f2c265bb727a950d7f14bcb6c51

---
+++ src/app/graph-editor/services/can-deactivate.guard.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate } from '@angular/router';
+import { Observable } from 'rxjs';
+import { GraphEditorContainerComponent } from '../container/graph-editor-container/graph-editor-container.component';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CanDeactivateGraphEditorGuard implements CanDeactivate<GraphEditorContainerComponent> {
+
+    canDeactivate(component: GraphEditorContainerComponent): Observable<boolean> | boolean {
+        return (component.routerEventIsPopstate() ? component.canDeactivate() : true);
+    }
+}
