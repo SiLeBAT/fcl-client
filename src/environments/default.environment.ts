d19a20158d756fed407b9aa76fa2cc738434f3eb
Birgit Lewicki <lewicki.birgit@gmail.com>
2019-11-22 18:14:41 +0100

    Add release version and date in footer.
    Ticket: https://app.zenhub.com/workspaces/fcl-5821aea8cbe1b0da7b31ee86/issues/silebat/fcl-client/237
    Change-Id: If5ed71e47ac6c8f76b00b1db8e8e3edef3cc99f3

--- src/environments/default.environment.ts
+++ src/environments/default.environment.ts
@@ -2,5 +2,7 @@ export const environment = {
     production: false,
     serverless: false,
     appName: 'FoodChain-Lab',
-    supportContact: 'foodrisklabs@bfr.bund.de'
+    supportContact: 'foodrisklabs@bfr.bund.de',
+    version: require('../../package.json').version,
+    lastChange: require('../../package.json').fclConfig.lastChange
 };
