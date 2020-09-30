445d3a396be1bb0890c697cd47554d8636d07adb
Marco Ruegen <marco.ruegen@bfr.bund.de>
2020-09-30 08:25:25 +0200

    gis refactoring: geomap-licref-view component added
    - this component displays a licence reference for open street map
    
    Change-Id: I2a406347d91d03085b67374ed44e651ab02b2ab5

---
+++ src/app/tracing/graph/components/geomap-licref/geomap-licref-view.component.ts
@@ -0,0 +1,12 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+    selector: 'fcl-geomap-licref-view',
+    templateUrl: './geomap-licref-view.component.html',
+    styleUrls: ['./geomap-licref-view.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class GeoMapLicRefViewComponent {
+
+    constructor() {}
+}
