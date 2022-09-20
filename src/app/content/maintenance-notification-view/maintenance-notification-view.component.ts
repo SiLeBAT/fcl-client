3611cc58d5c460d0e025166c6af313f3be644c51
Marco Ruegen <marco.ruegen@bfr.bund.de>
2022-09-20 07:55:14 +0200

    adds maintenance notification
    ticket: #685
    Change-Id: I50a45d3a9bfcc1a7332356f010868085976ee888

---
+++ src/app/content/maintenance-notification-view/maintenance-notification-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'fcl-maintenance-notification-view',
+    templateUrl: './maintenance-notification-view.component.html',
+    styleUrls: ['./maintenance-notification-view.component.scss']
+})
+export class MaintenanceNotificationViewComponent {
+
+}
