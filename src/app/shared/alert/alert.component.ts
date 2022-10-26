94aeb13d399664e43df40fabdc12976097397516
Marco Ruegen <marco.ruegen@bfr.bund.de>
2022-10-27 00:42:09 +0200

    removes auto dismiss from error messages
    previous mode: error message are auto dismissed
    new mode: error message contain an ok button, they
    are dismissed only if the user clicks on Ok other somewhere
    outside of the toaster box
    
    ticket: #510
    Change-Id: I0d512f302aa7017b9d8d4cdcee5b21a4114e705c

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarConfig, MatSnackBarRef, TextOnlySnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs';
 
 import { AlertService, INotification } from '../services/alert.service';
 
@@ -24,6 +25,33 @@ export class AlertComponent implements OnInit {
     showToaster(notification: INotification) {
         const text = notification.text;
         const config: MatSnackBarConfig = notification.config;
-        this.snackBar.open(text, '', config);
+        const autoDismiss = !!config.duration;
+        const snackBarRef = this.snackBar.open(text, !autoDismiss ? 'Ok' : '', config);
+
+        if (!autoDismiss) {
+            this.dismissSnackBarOnOutsideSnackBarClick(snackBarRef);
+        }
+    }
+
+    private dismissSnackBarOnOutsideSnackBarClick(snackBarRef: MatSnackBarRef<TextOnlySnackBar>): void {
+        const snackBarContainers = document.getElementsByClassName('mat-snack-bar-container');
+        if (snackBarContainers.length > 0) {
+            const snackBarContainer = snackBarContainers.item(snackBarContainers.length - 1);
+            const fun = (e: any) => {
+                if (!snackBarContainer.contains(e.target)) {
+                    // Clicked outside the snackbar box
+                    snackBarRef.dismiss();
+                }
+            };
+
+            let subscription: Subscription | null = null;
+            const dismissed = () => {
+                window.removeEventListener('click', fun);
+                subscription.unsubscribe();
+            };
+            subscription = snackBarRef.afterDismissed().subscribe(dismissed);
+
+            window.addEventListener('click', fun);
+        }
     }
 }
