ead711f7033eded688bf3a09229e1d665ff89277
Marco Ruegen <marco.ruegen@bfr.bund.de>
2022-11-25 08:59:34 +0100

    adds scroll editRule into view feature
    ticket: #707
    Change-Id: I0eca7ee25100e4a9fce20cd45cb89453dbb01e14

--- src/app/tracing/configuration/highlighting-rules-list-view/highlighting-rules-list-view.component.ts
+++ src/app/tracing/configuration/highlighting-rules-list-view/highlighting-rules-list-view.component.ts
@@ -1,5 +1,5 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, TemplateRef } from '@angular/core';
+import { AfterViewChecked, ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, OnChanges, Output, QueryList, SimpleChanges, TemplateRef, ViewChild, ViewChildren } from '@angular/core';
 import { HighlightingRuleDeleteRequestData } from '../configuration.model';
 import { EditRule, RuleId, RuleListItem } from '../model';
 
@@ -9,7 +9,7 @@ import { EditRule, RuleId, RuleListItem } from '../model';
     styleUrls: ['./highlighting-rules-list-view.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HighlightingRulesListViewComponent <T extends EditRule> implements OnChanges {
+export class HighlightingRulesListViewComponent <T extends EditRule> implements OnChanges, AfterViewChecked {
 
     @Input() showShowInLegendToggleButton = true;
     @Input() listItems: RuleListItem[] = [];
@@ -24,7 +24,12 @@ export class HighlightingRulesListViewComponent <T extends EditRule> implements 
     @Output() newRule = new EventEmitter<void>();
     @Output() startEdit = new EventEmitter<RuleId>();
 
+    @ViewChild('editRuleElement', { read: ElementRef, static: false }) editRuleElementRef: ElementRef;
+    @ViewChild('newRuleElement', { read: ElementRef, static: false }) newRuleElementRef: ElementRef;
+
     private isEditRuleNew = false;
+
+    private scrollEditRuleIntoViewAfterViewChecked = false;
 
     get showAddRuleButton(): boolean {
         return this.editRule === null || this.isEditRuleNew; // this.editRule.isNew;
@@ -41,6 +46,19 @@ export class HighlightingRulesListViewComponent <T extends EditRule> implements 
     ngOnChanges(changes: SimpleChanges): void {
         if (changes.editRule !== undefined || changes.listItems !== undefined) {
             this.isEditRuleNew = this.editRule !== null && !this.listItems.some(item => item.id === this.editRule.id);
+        }
+        if (changes.editRule !== undefined && this.editRule !== null) {
+            const prevEditRule = (changes.editRule.previousValue as EditRule | null);
+            if (prevEditRule === null || prevEditRule.id !== this.editRule.id) {
+                // editrule was opened
+                this.scrollEditRuleIntoViewAfterViewChecked = true;
+            }
+        }
+    }
+
+    ngAfterViewChecked(): void {
+        if (this.scrollEditRuleIntoViewAfterViewChecked) {
+            this.scrollEditRuleIntoView();
         }
     }
 
@@ -73,6 +91,28 @@ export class HighlightingRulesListViewComponent <T extends EditRule> implements 
             const newRuleIdOrder = this.listItems.map(item => item.id);
             moveItemInArray(newRuleIdOrder, event.previousIndex, event.currentIndex);
             this.ruleOrderChange.emit(newRuleIdOrder);
+        }
+    }
+
+    private scrollEditRuleIntoView(): void {
+        const element = this.newRuleElementRef ?? this.editRuleElementRef;
+        if (element) {
+            const htmlElement = element.nativeElement as HTMLElement;
+            const boundingElement = htmlElement.closest('.mat-tab-body-content');
+            const boundingRect = boundingElement.getBoundingClientRect();
+            const elementRect = htmlElement.getBoundingClientRect();
+            const topDist = elementRect.top - boundingRect.top;
+            const bottomDist = boundingRect.bottom - elementRect.bottom;
+            const scrollBy = (
+                topDist < 0 ? topDist :
+                    bottomDist < 0 ? Math.min(topDist, -bottomDist) :
+                        0
+            );
+
+            if (scrollBy !== 0) {
+                boundingElement.scrollBy(0, scrollBy);
+            }
+            this.scrollEditRuleIntoViewAfterViewChecked = false;
         }
     }
 }
