d1223fa43b296815ec8b2b6b766e235a8197bfd3
Marco Ruegen <marco.ruegen@bfr.bund.de>
2023-11-17 18:01:19 +0100

    adds feature post build json minimization
    - adds dev-dep 'glob' to find json files
    - adds postbuild npm script to minimize json files
    - refines npm scripts build:* to call postbuild step
    - adds js file to minimize files
    - removes npm script build:qa_wo_lint
    
    - adds linting rules for *.js files to .eslintrc.json
    - prettifies scenario example files
    
    ticket: #712, #752
    Change-Id: I78f7b05e80bd3d67238656428d2fdaab2dd2788b

---
+++ build-utils/post-build-minify-json.js
@@ -0,0 +1,16 @@
+const { globSync } = require('glob');
+const { readFileSync, writeFileSync } = require('fs');
+
+if (process.argv.length < 3) {
+    throw new Error(`Json Glob pattern is missing!`);
+}
+
+const globPattern = process.argv[2];
+const jsonFiles = globSync(globPattern);
+
+function minifyJson(filepath) {
+    const jsonStr = readFileSync(filepath);
+    writeFileSync(filepath, JSON.stringify(JSON.parse(jsonStr)), { encoding: 'utf8' });
+}
+
+jsonFiles.forEach(filepath => minifyJson(filepath));
