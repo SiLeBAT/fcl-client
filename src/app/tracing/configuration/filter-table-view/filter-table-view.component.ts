dd7ce868e91f3de8a8dc4a5a5c0e889b5df8315a
arne-z <38384819+arne-z@users.noreply.github.com>
2024-08-22 12:17:27 +0200

    Data table for deliveries: Outbreak column empty
    * Show Delivery outbreak status in table view.
    
    * Remove guard in filtertableview onRowSelectionChange as it it oos for this issue.
    
    * Fix linter errors.
    
    Ticket: #788

--- src/app/tracing/configuration/filter-table-view/filter-table-view.component.ts
+++ src/app/tracing/configuration/filter-table-view/filter-table-view.component.ts
@@ -150,7 +150,7 @@ export class FilterTableViewComponent implements OnChanges, DoCheck, OnInit, OnD
     constructor(
         private hostElement: ElementRef,
         private cdRef: ChangeDetectorRef
-    ) {}
+    ) { }
 
     // lifecycle hooks start
 
@@ -167,14 +167,14 @@ export class FilterTableViewComponent implements OnChanges, DoCheck, OnInit, OnD
             this.subscriptions_.push(this.activityState$.subscribe(
                 (activityState) => this.setActivityState(activityState),
                 // eslint-disable-next-line @typescript-eslint/no-empty-function
-                () => {}
+                () => { }
             ));
         }
         if (this.checkTableSize$ !== undefined && this.checkTableSize$ !== null) {
             this.subscriptions_.push(this.checkTableSize$.subscribe(
                 () => this.checkTableSizeOnStableWrapperSize({ stopOnOpen: false }),
                 // eslint-disable-next-line @typescript-eslint/no-empty-function
-                () => {}
+                () => { }
             ));
         }
         if (this.updateTableSize$ !== undefined && this.updateTableSize$ !== null) {
@@ -186,7 +186,7 @@ export class FilterTableViewComponent implements OnChanges, DoCheck, OnInit, OnD
                     }
                 },
                 // eslint-disable-next-line @typescript-eslint/no-empty-function
-                () => {}
+                () => { }
             ));
         }
     }
@@ -490,7 +490,7 @@ export class FilterTableViewComponent implements OnChanges, DoCheck, OnInit, OnD
             }
         };
         // eslint-disable-next-line @typescript-eslint/no-empty-function
-        asyncTask.subscription = this.cycleStart$!.subscribe(callBack, () => {});
+        asyncTask.subscription = this.cycleStart$!.subscribe(callBack, () => { });
         this.asyncTasks_.push(asyncTask as AsyncTask);
     }
 
@@ -678,7 +678,7 @@ export class FilterTableViewComponent implements OnChanges, DoCheck, OnInit, OnD
             }
         }
         // eslint-disable-next-line no-empty
-        if (this.processedInput__ !== this.inputData) {}
+        if (this.processedInput__ !== this.inputData) { }
     }
 
     private applyTreeStatusFromCache(rows: TableRow[]): void {
@@ -855,7 +855,7 @@ export class FilterTableViewComponent implements OnChanges, DoCheck, OnInit, OnD
         ];
     }
 
-    private getCellClass({ row }: { row: TableRow; column: NgxTableColumn; value: any}): any {
+    private getCellClass({ row }: { row: TableRow; column: NgxTableColumn; value: any }): any {
         return {
             'fcl-row-cell-invisible': row['invisible'] || (row.parentRow !== undefined && row.parentRow['invisible']),
             'fcl-font-style-italic': row.parentRowId !== undefined
