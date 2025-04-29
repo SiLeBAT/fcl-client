c3e7a57f1e2781a856d04fbd1afa0e0091bcc442
Marco Ruegen <marco.ruegen@bfr.bund.de>
2025-04-29 12:47:36 +0200

    fixes placeholder and autodisabling of highlighting rules
    fixes:
    - placeholder for property selector
    - auto disabling of label rules on model load
    
    ticket: #948, #991

--- src/app/tracing/io/data-importer/data-importer-v1.ts
+++ src/app/tracing/io/data-importer/data-importer-v1.ts
@@ -54,6 +54,7 @@ import { Constants } from "../../util/constants";
 import { PartialPick } from "../../../tracing/util/utility-types";
 import { isValidJsonSchemaV7 } from "./json-validation/json-schema-validation";
 import { createInitialFclDataState } from "../../state/tracing.reducers";
+import { isSimpleLabelHRule } from "../../configuration/shared";
 
 const JSON_SCHEMA_FILE = "../../../../assets/schema/schema-v1.json";
 
@@ -778,9 +779,11 @@ export class DataImporterV1 implements IDataImporter {
             : createDefaultStationAnonymizationLabelHRule();
 
         if (intStatAnoHRule.userDisabled === false) {
-            fclData.graphSettings.highlightingSettings.stations.forEach(
-                (r) => (r.autoDisabled = true),
-            );
+            fclData.graphSettings.highlightingSettings.stations.forEach((r) => {
+                if (isSimpleLabelHRule(r)) {
+                    r.autoDisabled = true;
+                }
+            });
         }
 
         fclData.graphSettings.highlightingSettings.stations.push(
