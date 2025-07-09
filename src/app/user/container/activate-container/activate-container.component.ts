93ce1597aa99c7f8dc52d3af8b7c7572a7e04e31
Marco Ruegen <marco.ruegen@bfr.bund.de>
2025-07-09 17:28:26 +0200

    replaces users/activate from history & adds login btn to activation
    - after leaving users/activate the link users/activate/.. is replaced by
      users/login
    - a login button is added to activation template
    - updates activate template snapshot
    
    ticket: #996

--- src/app/user/container/activate-container/activate-container.component.ts
+++ src/app/user/container/activate-container/activate-container.component.ts
@@ -1,31 +1,47 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { environment } from "../../../../environments/environment";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, NavigationStart, Router } from "@angular/router";
 
 import { UserService } from "../../services/user.service";
 import { AlertService } from "../../../shared/services/alert.service";
 import { SpinnerLoaderService } from "../../../shared/services/spinner-loader.service";
 import { ActivationResponseDTO } from "@app/user/models/user.model";
+import { Subscription } from "rxjs";
+import { GuardedUnloadDirective } from "@app/shared/container/guarded-unload.directive";
+import { filter } from "rxjs/operators";
 
 @Component({
     selector: "fcl-activate-container",
     templateUrl: "./activate-container.component.html",
 })
-export class ActivateContainerComponent implements OnInit {
+export class ActivateContainerComponent
+    extends GuardedUnloadDirective
+    implements OnInit, OnDestroy
+{
     tokenValid: boolean;
     appName: string = environment.appName;
 
+    private routerSubscription_: Subscription | undefined;
+
     constructor(
+        private router: Router,
         private activatedRoute: ActivatedRoute,
         private userService: UserService,
         private alertService: AlertService,
         private spinnerService: SpinnerLoaderService,
-    ) {}
+    ) {
+        super();
+    }
 
     ngOnInit() {
         const token = this.activatedRoute.snapshot.params["id"];
 
         this.spinnerService.show();
+
+        this.routerSubscription_ = this.router.events
+            .pipe(filter((event) => event instanceof NavigationStart))
+            .subscribe((event) => this.replaceHistoryState());
+
         this.userService.activateAccount(token).subscribe(
             (activationResponse: ActivationResponseDTO) => {
                 this.spinnerService.hide();
@@ -39,5 +55,26 @@ export class ActivateContainerComponent implements OnInit {
                 this.tokenValid = false;
             },
         );
+    }
+
+    ngOnDestroy(): void {
+        this.cleanRouterSubscription();
+    }
+
+    unloadGuard(): boolean {
+        this.replaceHistoryState();
+        return true;
+    }
+
+    private replaceHistoryState(): void {
+        history.replaceState(null, "", "/users/login");
+        this.cleanRouterSubscription();
+    }
+
+    private cleanRouterSubscription(): void {
+        if (this.routerSubscription_) {
+            this.routerSubscription_.unsubscribe();
+            this.routerSubscription_ = undefined;
+        }
     }
 }
